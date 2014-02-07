5d676749f92d63d265126e8d0c29974a8af4add3
AlviMurtaza <smurtazama@gmail.com>
2014-02-07 17:56:30 +0530

    Updates

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,10 @@
 class PostsController < ApplicationController
   def index
+     @posts = Post.all
+  end
+
+  def create
+    Post.create params[:title][:body]
+    redirect_to :back
   end
 end
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -3,8 +3,15 @@ require 'spec_helper'
 describe "Posts" do
   describe "GET /posts" do
     it "displays some posts" do
+        @post = Post.create :title => 'My first Post', :body => 'Lets fineish work Quickly'
         visit posts_path
         page.should have_content 'First Video'
     end
+    it "creates new post" do
+        visit posts_path
+        fill_in 'Post', :with => 'New Video'
+        click_button 'Create Post'
+        current_path.should == posts_path
+        page.should have_content 'Second Video'
   end
 end
